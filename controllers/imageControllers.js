b3ba7bf703ee8152287b20c525681b1bc26fa91a
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-01-16 15:55:21 +0100

    Quick fix for multer error handling

--- controllers/imageControllers.js
+++ controllers/imageControllers.js
@@ -1,6 +1,9 @@
 const short = require('short-uuid');
-// const multer = require('multer')
-// const upload = multer({ dest: 'uploads' }); // upload without local storage
+const multer = require('multer')
+// const upload = multer({ dest: 'uploads' }); // upload without local storage 1
+
+const upload = multer({ dest: 'uploads', limits: { fileSize: 1024 * 1024 } }).single('image'); // upload without local storage 2
+
 const path = require('path')
 
 const Image = require('../models/Image')
@@ -39,57 +42,62 @@ exports.getImage = async (req, res) => {
 exports.uploadImage = async (req, res) => {
     // res.send("Uploaded image")
 
-    // upload without local storage
-    try {
-        const newImage = {
-            imageId: short.generate(),
-            originalName: req.file.originalname,
-            path: req.file.path
-        }
-        const sentImage = await Image.create(newImage);
-        return res.status(201).json({
-            message: "Image uploaded successfully",
-            sentImage
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            "message": "Error uploading image",
-            "error": error
-        })
-    }
-
-    // upload(req, res, async function (err) {
-    //     if (err instanceof multer.MulterError) {
-
-    //       // A Multer error occurred when uploading.
-    //         return res.status(400).send("File must not be more than 1mb")
-    //     } else if (err) {
-
-    //       // An unknown error occurred when uploading.
-    //       return res.status(400).send("Another unknown Error" + err)
+    // upload without local storage 1
+    // try {
+    //     if (req.uploadError) {
+    //         return res.send("Multer Error don dey:" + req.uploadError)
     //     }
-
-    //     // Everything went fine.
     //     const newImage = {
     //         imageId: short.generate(),
     //         originalName: req.file.originalname,
     //         path: req.file.path
     //     }
-    //     try {
-    //         const sentImage = await Image.create(newImage);
-    //         return res.status(201).json({
-    //             message: "Image uploaded successfully",
-    //             sentImage
-    //         })
-    //     } catch (error) {
-    //         console.log(error.message)
-    //         res.status(500).json({
-    //             message: "Internal error",
-    //             error
-    //         })
-    //     }
-    //   })
+    //     const sentImage = await Image.create(newImage);
+    //     return res.status(201).json({
+    //         message: "Image uploaded successfully",
+    //         sentImage
+    //     })
+    // } catch (error) {
+    //     console.log(error)
+    //     return res.status(500).json({
+    //         "message": "Error uploading image",
+    //         "error": error
+    //     })
+    // }
+
+    // upload without local storage 2
+
+    upload(req, res, async function (err) {
+        if (err instanceof multer.MulterError) {
+
+          // A Multer error occurred when uploading.
+            return res.status(400).send("File must not be more than 1mb")
+        } else if (err) {
+
+          // An unknown error occurred when uploading.
+          return res.status(400).send("Another unknown Error" + err)
+        }
+
+        // Everything went fine.
+        const newImage = {
+            imageId: short.generate(),
+            originalName: req.file.originalname,
+            path: req.file.path
+        }
+        try {
+            const sentImage = await Image.create(newImage);
+            return res.status(201).json({
+                message: "Image uploaded successfully",
+                sentImage
+            })
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({
+                message: "Internal error",
+                error
+            })
+        }
+    })
 }
 
 
--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const imageRoutes = express.Router();
 
-const multer = require('multer')
-const upload = multer({ dest: 'uploads', limits: { fileSize: 1024 * 1024 } }); // upload without local storage
+// const multer = require('multer')
+// const upload = multer({ dest: 'uploads', limits: { fileSize: 1024 * 1024 } }); // upload without local storage
 
+const handleMulterErr = require('../middlewares/handleMulterErr')
 
 const authAccess = require('../middlewares/authAccess')
 
@@ -11,6 +12,10 @@ const imageControllers = require('../controllers/imageControllers')
 
 imageRoutes
     .get('/image/:id', authAccess, imageControllers.getImage)
-    .post('/image/upload', authAccess, upload.single('image'), imageControllers.uploadImage)
+    // .post('/image/upload', authAccess, upload.single('image'), imageControllers.uploadImage)
+    // .post('/image/upload', authAccess, handleMulterErr, imageControllers.uploadImage)
+    .post('/image/upload', authAccess, imageControllers.uploadImage)
+
+
 
 module.exports = imageRoutes;
