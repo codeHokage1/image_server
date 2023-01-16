a987df44cf10fb87144077c1c7ab420643a94680
Sodiq Farhan <sodiqfarhan360@gmail.com>
2023-01-16 16:02:43 +0100

    Quick fix for multer error handling

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -4,8 +4,6 @@ const imageRoutes = express.Router();
 // const multer = require('multer')
 // const upload = multer({ dest: 'uploads', limits: { fileSize: 1024 * 1024 } }); // upload without local storage
 
-const handleMulterErr = require('../middlewares/handleMulterErr')
-
 const authAccess = require('../middlewares/authAccess')
 
 const imageControllers = require('../controllers/imageControllers')
